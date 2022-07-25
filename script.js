2aa8abee57d5d422beab1d4739b1ac3ecd3abc3a
Nobel <nobel_lamun@hotmail.com>
2022-07-25 18:19:43 -0230

    added UI

--- script.js
+++ script.js
@@ -1,76 +1,114 @@
-const choices = ["rock", "paper", "scissors"];
-const winners = []
-
-function game(){
-    for(i = 1; i <= 5; i++){
-    playRound(i)};
-    logWins();
-}
-
-function computerPlay(){
-    let randomNumber = Math.floor(Math.random()*choices.length);
-    return choices[randomNumber]
-}
-
-function playerChoice(){
-    let input = prompt("Type rock, paper or scissors")
-    while (input == null){
-       input = prompt("Type rock, paper or scissors");
-    }
-    input = input.toLowerCase();
-    let check = validateInput(input);
-    while (check==false){
-       input = prompt("Type rock, paper or scissors. Nothing else!");
-       while (input == null){
-        input = prompt("Type rock, paper or scissors");
-       }
-    input = input.toLowerCase();
-    check = validateInput(input);
-}
-return input;
-}
-
-function playRound(round){
-    const playerSelection = playerChoice();
-    const computerSelection = computerPlay();
-    const winner = checkWinner(playerSelection, computerSelection);
-    winners.push(winner);
-    logRound(playerSelection, computerSelection, winner, round)
-}
+//spans first!
+const computerChoiceDisplay = document.getElementById("computer-choice")
+const playerChoiceDisplay = document.getElementById("player-choice")
+const resultDisplay = document.getElementById("result")
+const computerScore = document.getElementById("computerScore")
+const playerScore = document.getElementById("playerScore")
+const winnerDisplay = document.getElementById("winner")
+const restartDisplay = document.getElementById("restart")
+let userChoice
+let computerChoice
+let result
+let playerPoint = 0;
+let computerPoint = 0;
 
 
-function validateInput(x){
-    return choices.includes(x)
-}
+//now the possible choices
+const possibleUserChoices = document.querySelectorAll(".pImg")
+const possibleComputerChoices = document.querySelectorAll(".butt")
 
-function checkWinner(choiceP, choiceC) {
-    if (choiceP === choiceC) {
-      return "Tie";
-    } else if (
-      (choiceP === "rock" && choiceC == "scissors") ||
-      (choiceP === "paper" && choiceC == "rock") ||
-      (choiceP === "scissors" && choiceC == "paper")
+
+//now make the user and computer choices do something!
+possibleUserChoices.forEach(possibleChoice => possibleChoice.addEventListener("click", (e)=>{
+   userChoice = e.target.id
+   playerChoiceDisplay.innerHTML = userChoice;
+//fight computer now that we have our choice
+   MakeComputerChoice()
+//see who wins
+   getResult()
+
+
+   
+   if (
+    (playerPoint === 5 || computerPoint === 5)
     ) {
-      return "Player";
-    } else {
-      return "Computer";
+    //display end results
+    //change the button to visible,
+    //change the text to display winner
+    displayEnd();
+  }
+}))
+
+
+function MakeComputerChoice() {//computer chooses randomly and then we associate it with "number===rockpaper or scissors"
+    const choice = Math.floor(Math.random()*possibleComputerChoices.length)
+  
+    if (choice === 0){
+        computerChoice = "rock"
     }
-  }
+    if (choice === 1){
+        computerChoice = "paper"
+    }
+    if (choice === 2){
+        computerChoice = "scissors"
+    }
 
-  function logWins(){
-      let playerWins = winners.filter((item) => item == "Player");
-      let computerWins = winners.filter((item) => item == "Computer");
-      let ties = winners.filter((item) => item == "Tie");
-      console.log("Results:")
-      console.log("Player Wins:", playerWins);
-      console.log("Computer Wins:", computerWins);
-      console.log("Ties:", ties);
-  }
+    computerChoiceDisplay.innerHTML = computerChoice
+    }
 
-  function logRound(playerChoice, computerChoice, winner, round){
-      console.log("Round", round)
-      console.log("Player Chose:", playerChoice)
-      console.log("Computer Chose:", computerChoice)
-      console.log(winner, "Won the round!")
-      console.log("---------------------------------")
-  }
+//game rules!
+function getResult(){
+    if (userChoice === computerChoice) {
+        result = "Tie!";
+    }
+    else if (
+        (userChoice === "rock" && computerChoice === "scissors") ||
+        (userChoice === "paper" && computerChoice === "rock") ||
+        (userChoice === "scissors" && computerChoice === "paper")
+       ) 
+    { 
+        playerPoint += 1;
+        result = "You win!";
+    } else { 
+        computerPoint += 1;
+        result = "You lose!";
+    }
+
+    if (playerPoint > 5 || computerPoint > 5) {
+        return;
+    }
+    playerScore.innerHTML = playerPoint
+    computerScore.innerHTML = computerPoint
+    resultDisplay.innerHTML = result
+}
+
+
+//show winner
+function displayEnd(){
+if (playerPoint === 5) {
+    winnerDisplay.textContent = "You won THE GAME!!"
+}
+if (computerPoint === 5) {
+    winnerDisplay.textContent = "You lose the GAME!!"
+};
+//MAKE RESTART APPEAR 
+restartDisplay.classList.remove("disable");
+
+//Stop player from playing more than 5 times  
+possibleUserChoices.forEach(possibleChoice => possibleChoice.removeEventListener("click", (e)=>{
+    userChoice = e.target.id
+    playerChoiceDisplay.innerHTML = userChoice
+}))
+
+}
+
+
+
+
+//Make restart button restart the game
+restartDisplay.addEventListener("click", () =>{
+window.location.reload();
+}
+)
+
+
